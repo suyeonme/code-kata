be783f739c1a98f06ec31e788563de35c5ce262d
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-03 11:03:13 +0900

    Problem 1

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/*  
+* Find Kim
+*/
+const solution = (city) => {
+    const index = city.indexOf('Kim')
+    return console.log(`김서방은 ${index}에 있다`);    
+};
+const seoul = ['Lee', 'James', 'Kim', 'Kang'];
+solution(seoul);
+
+
+/*  
+* Average number
+*/
+const solution = arr => {
+    return arr.reduce((total, cur) => total + cur) / arr.length;
+};
+const arr = [1, 2, 3, 4];
+console.log('Average: ' + solution(arr));
+
