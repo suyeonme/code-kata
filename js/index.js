04623e089de1697f3316bc5a91b76e9062d23e1e
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-05 13:13:33 +0900

    Solving problem 6: The number of K

--- js/index.js
+++ js/index.js
@@ -179,3 +179,19 @@ aboutMe();
     nextQuestion();
 })();
 */
+
+
+/*
+* The number of K (June 5, 2020)
+*
+function solution(array, commands) {
+    return commands.map(v => {
+        return array.slice(v[0] - 1, v[1]).sort((a, b) => a - b).slice(v[2] - 1, v[2])[0];
+    });
+}
+
+const arr = [1, 5, 2, 6, 3, 7, 4]
+solution(arr, [[2, 5, 3], [4, 4, 1], [1, 7, 3]]);
+
+// Return : [5, 6, 3]
+*/
