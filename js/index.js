9fe70eda93c05c0397a43c8d5d9ca66297e6b630
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-15 09:59:59 +0900

    Who get the most answers? (프로그래머스 모의고사)

--- js/index.js
+++ js/index.js
@@ -1,21 +1,58 @@
-/* Dec 12, 2020
-숫자로 이루어진 배열인 nums를 인자로 전달합니다.
-숫자중에서 과반수(majority, more than a half)가 넘은 숫자를 반환해주세요.
+/* Dec 15, 2020
+Programmers 모의고사
 
-nums = [2,2,1,1,1,2,2]
-return 2
-
-nums 배열의 길이는 무조건 2개 이상입니다.
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
 */
 
-const nums = [2, 2, 1, 1, 1, 2, 2];
+function solution(answers) {
+  const answer = []; // 가장 많은 문제를 맞힌 사람
 
-const findMajority = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.filter(el => el === arr[i]).length > arr.length / 2) {
-      return arr[i];
+  let count = [0, 0, 0];
+  const p1 = [1, 2, 3, 4, 5];
+  const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === p1[i % p1.length]) {
+      count[0]++;
+    }
+    if (answers[i] === p2[i]) {
+      count[1]++;
+    }
+    if (answers[i] === p3[i]) {
+      count[2]++;
     }
   }
-};
 
-findMajority(nums);
+  for (let i = 0; i < count.length; i++) {
+    let max = Math.max(...count);
+    if (count[i] === max) {
+      answer.push(i + 1);
+    }
+  }
+  return answer;
+}
+
+const answerArr = [1, 3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2, 4, 2];
+solution(answerArr);
+
+/*
+(1): Modulus(Remainder)
+    0 % 5 === 0
+    1 % 5 === 1
+    2 % 5 === 2
+    4 % 2 === 0;
+
+(2): Math.max
+    Math.max(1, 3, 2)
+    Math.max(...array1)
+
+(3): p1[i % p1.length]
+    수포자의 배열이 정답 배열보다 짧은 경우, 올바른 index값을 얻을 수 있음 !!
+*/
+
+// answers = [1,2,3,4,5]
+// p1 = [1,2,3]
+// answers[4] === p1[4 % p1.length] -> p1[1]
