1f3c3c050e177ed715cc193b3eabe3f5b367b645
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-20 12:27:30 +0900

    Programmers, 체육복- another solution

--- js/index.js
+++ js/index.js
@@ -1,47 +1,35 @@
-/* Dec 16, 2020
+/* 
 Programmers 체육복
 */
 
-const solution = (n, lost, reserve) => {
+function solution(n, lost, reserve) {
   let answer = 0;
-  const trueLost = lost.filter((l, i) => l !== reserve[i]);
-  const trueReserve = reserve.filter((l, i) => l !== lost[i]);
-  let ans = [];
+  const ans = [];
 
-  for (let i = 0; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     ans.push(1);
   }
 
-  for (let i = 1; i <= n; i++) {
-    for (let j = 0; j < trueLost.length; j++) {
-      if (i === lost[j]) ans[i] = 0;
-    }
-    for (let j = 0; j < trueReserve.length; j++) {
-      if (i === reserve[j]) ans[i] += 1;
-    }
-  }
+  lost.forEach(number => (ans[number] -= 1));
+  reserve.forEach(number => (ans[number] += 1));
 
   for (let i = 1; i <= n; i++) {
-    if (ans[i] === 0 && ans[i - 1] === 2) {
-      ans[i - 1] = 1;
-      ans[i] = 1;
-    } else if (ans[i] === 0 && ans[i + 1] === 2) {
-      ans[i + 1] = 1;
-      ans[i] = 1;
+    if (ans[i] === 2 && ans[i - 1] === 0) {
+      ans[i - 1]++;
+      ans[i]--;
+    } else if (ans[i] === 2 && ans[i + 1] === 0) {
+      ans[i + 1]++;
+      ans[i]--;
     }
   }
 
-  for (let i = 1; i <= n; i++) {
-    if (ans[i] > 0) answer++;
+  for (let i = 0; i <= n; i++) {
+    if (ans[i] >= 1) answer++;
   }
-
-  console.log(answer);
   return answer;
-};
+}
 
 const n = 5;
 const lost = [2, 4];
 const reserve = [1, 3, 5];
 solution(n, lost, reserve);
-
-// For loop (i)
