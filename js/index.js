5fdc4c5320f48f9b10f653c7a691f03668449941
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-21 21:53:44 +0900

    Programmers, 2016

--- js/index.js
+++ js/index.js
@@ -1,35 +1,19 @@
 /* 
-Programmers 체육복
+Programmers 2016년
+
+2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 
+두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 
+요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 
+예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
 */
 
-function solution(n, lost, reserve) {
-  let answer = 0;
-  const ans = [];
-
-  for (let i = 1; i <= n; i++) {
-    ans.push(1);
-  }
-
-  lost.forEach(number => (ans[number] -= 1));
-  reserve.forEach(number => (ans[number] += 1));
-
-  for (let i = 1; i <= n; i++) {
-    if (ans[i] === 2 && ans[i - 1] === 0) {
-      ans[i - 1]++;
-      ans[i]--;
-    } else if (ans[i] === 2 && ans[i + 1] === 0) {
-      ans[i + 1]++;
-      ans[i]--;
-    }
-  }
-
-  for (let i = 0; i <= n; i++) {
-    if (ans[i] >= 1) answer++;
-  }
+function solution(m, d) {
+  var answer = '';
+  const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const date = new Date(`2016, ${m}, ${d}`);
+  answer = day[date.getDay()];
+  console.log(answer);
   return answer;
 }
 
-const n = 5;
-const lost = [2, 4];
-const reserve = [1, 3, 5];
-solution(n, lost, reserve);
+solution(5, 24);
