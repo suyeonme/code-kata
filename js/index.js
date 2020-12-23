9299a499ae3e25fcfc059ad1b529f932520ae244
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-23 11:03:49 +0900

    Programmers, get a middle number

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 /* 
-Programmers 2016년
+Programmers 가운데 글자 가져오기
 
-2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 
-두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 
-요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 
-예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+s: abcde	
+return c
+
+s: qwer
+return we	
 */
 
-function solution(m, d) {
-  var answer = '';
-  const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-  const date = new Date(`2016, ${m}, ${d}`);
-  answer = day[date.getDay()];
-  console.log(answer);
-  return answer;
+function solution(s) {
+  let index = Math.ceil(s.length / 2) - 1;
+  const oddNum = s.length % 2 !== 0;
+
+  return oddNum ? s.substr(index, 1) : s.substr(index, 2);
 }
 
-solution(5, 24);
+solution('abcde');
+
+// substr(start, length)
