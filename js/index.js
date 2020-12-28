7dd39869b3de04dafcc18372e01344c758eb3505
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-28 10:09:38 +0900

    Remove duplicate values from an array

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 /* 
-Programmers 가운데 글자 가져오기
+Programmers 같은 숫자는 싫어
 
+배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
+이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다.
 
-단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
-단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
-
-s: abcde	
-return c
-
-s: qwer
-return we	
+예를 들면,
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
 */
 
-function solution(s) {
-  let index = Math.ceil(s.length / 2) - 1;
-  const oddNum = s.length % 2 !== 0;
-
-  return oddNum ? s.substr(index, 1) : s.substr(index, 2);
+function solution(arr) {
+  return arr.filter((el, i) => el !== arr[i + 1]);
 }
 
-solution('abcde');
+const nums = [4, 4, 4, 3, 3];
+solution(nums);
 
-// substr(start, length)
+/* Remove duplicate value
+  (1) arr = [...new Set(arr)];
+  (2) arr = arr.filter ((value, index, array) => array.indexOf (value) == index);
+*/
