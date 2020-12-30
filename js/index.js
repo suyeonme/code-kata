bce75ef9dfbff52974ebd2087d9a127842bc2644
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-30 09:47:10 +0900

    Sort string in descending order

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
 /* 
-Programmers 문자열 내 p와 y의 개수
+Programmers 문자열 내림차순으로 배치하기
 
-대문자와 소문자가 섞여있는 문자열 s가 주어집니다. 
-s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 
-'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 
-단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+Zbcdefg -> gfedcbZ
 */
 
 function solution(s) {
-  return s.match(/p/gi)?.length === s.match(/y/gi)?.length;
+  return s
+    .split('')
+    .sort((a, b) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+    })
+    .join('');
 }
 
 function solution2(s) {
-  return (
-    s.toUpperCase().split('P').length === s.toUpperCase().split('Y').length
-  );
+  return s.split('').sort().reverse().join('');
 }
 
-const str = 'Pyy';
+const str = 'Zbcdefg';
 solution(str);
