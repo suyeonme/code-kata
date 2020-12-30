1c0845a0ca20eaea73a0276c1ad4253510f2b9fb
Suyeon Kang <tndusrkd92@gmail.com>
2020-12-30 10:25:27 +0900

    문자열 다루기 기본

--- js/index.js
+++ js/index.js
@@ -1,25 +1,18 @@
 /* 
-Programmers 문자열 내림차순으로 배치하기
+Programmers 문자열 다루기 기본
 
-문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
-s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
-
-Zbcdefg -> gfedcbZ
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
 */
 
 function solution(s) {
-  return s
-    .split('')
-    .sort((a, b) => {
-      if (a > b) return -1;
-      if (a < b) return 1;
-    })
-    .join('');
+  return (s.length === 4 || s.length === 6) && s.match(/[A-Z]/gi)?.length > 0
+    ? false
+    : true;
 }
 
 function solution2(s) {
-  return s.split('').sort().reverse().join('');
+  return (s.length == 4 || s.length == 6) && isNaN(s) == false ? true : false;
 }
 
-const str = 'Zbcdefg';
-solution(str);
+solution('1234');
