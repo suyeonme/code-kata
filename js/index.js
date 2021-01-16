f9c2752fa1f72e2db0512ad1334f9bcce8054005
Suyeon Kang <tndusrkd92@gmail.com>
2021-01-16 11:52:06 +0900

    Programmers, 정수 내림차순으로 배치하기

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 /* 
-Programmers 자연수 뒤집어 배열로 만들기
+Programmers 정수 내림차순으로 배치하기
 
-자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
-예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+함수 solution은 정수 n을 매개변수로 입력받습니다. 
+n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+예를들어 n이 118372면 873211을 리턴하면 됩니다.
 */
 
 function solution(n) {
-  const reversed = n.toString().split('').reverse();
-  return reversed.map(s => +s);
+  const arr = n
+    .toString()
+    .split('')
+    .sort((a, b) => b - a);
+  return +arr.join('');
 }
 
-solution(12345);
+solution(118372);
