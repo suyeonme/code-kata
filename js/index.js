b97104608e125a0744465e964b952e5d0fff3bc7
Suyeon Kang <tndusrkd92@gmail.com>
2021-01-16 12:20:05 +0900

    Programmers, 정수 제곱근 판별

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 /* 
-Programmers 정수 내림차순으로 배치하기
+Programmers 정수 제곱근 판별
 
-함수 solution은 정수 n을 매개변수로 입력받습니다. 
-n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
-예를들어 n이 118372면 873211을 리턴하면 됩니다.
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+121 -> 144
+3 -> -1
+
 */
 
 function solution(n) {
-  const arr = n
-    .toString()
-    .split('')
-    .sort((a, b) => b - a);
-  return +arr.join('');
+  let x = Math.sqrt(n);
+  return Number.isInteger(x) ? Math.pow(x + 1, 2) : -1;
 }
 
-solution(118372);
+solution(121);
+
+/*
+  (1) Math.sqrt(n): Returns the square root of a number.
+  (2) Math.pow(base, exponent): Returns the base to the exponent power.
+  (3) Number.isInteger(n): Determines whether the passed value is an integer.
+*/
