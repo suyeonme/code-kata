b2bda96cdc2fd5d49520f70d5d435bb7a13e75ba
Suyeon Kang <tndusrkd92@gmail.com>
2021-01-19 11:25:10 +0900

    Programmers, 행렬의 덧셈

--- js/index.js
+++ js/index.js
@@ -1,21 +1,39 @@
 /* 
-Programmers 하샤드 수
+Programmers 행렬의 덧셈
 
-양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 
-예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 
-자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 
+2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 
-10 -> true
-11 -> false
+[[1,2],[2,3]], [[3,4],[5,6]] --> [[4,6],[7,9]]
+[[1],[2]], [[3],[4]] --> [[4],[6]]
 */
 
-function solution(n) {
-  const sum = n
-    .toString()
-    .split('')
-    .reduce((acc, cur) => (acc += +cur), 0);
+function solution1(arr1, arr2) {
+  let answer = [];
 
-  return n % sum === 0 ? true : false;
+  for (let i = 0; i < arr1.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      temp.push(arr1[i][j] + arr2[i][j]);
+    }
+    answer.push(temp);
+  }
+  return answer;
 }
 
-solution(18);
+function solution2(arr1, arr2) {
+  return arr1.map((_, i) => arr2[i].map((_, j) => arr1[i][j] + arr2[i][j]));
+}
+
+const arr1 = [
+  [1, 2],
+  [2, 3],
+];
+const arr2 = [
+  [3, 4],
+  [5, 6],
+];
+solution1(arr1, arr2);
+solution2(arr1, arr2);
+
+// [[4,6],[7,9]]
