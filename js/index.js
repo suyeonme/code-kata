27f97a078951e4ed044c59955b383ca8f497be04
Suyeon Kang <tndusrkd92@gmail.com>
2021-01-31 23:27:17 +0900

    Programmers, 위장

--- js/index.js
+++ js/index.js
@@ -1,32 +1,24 @@
-/* 
-Programmers 기능개발
+/* Programmers, 위장
+  Hash Table
+  서로 다른 옷의 조합의 수(모든 경우의 수)를 return
 */
 
-function solution(progresses, speeds) {
-  let answer = [];
+const solution = clothes => {
+  // 옷의 가짓수 세기
+  const closet = clothes.reduce((a, c) => {
+    a[c[1]] = a[c[1]] ? a[c[1]] + 1 : 1;
+    return a;
+  }, {});
 
-  const leftDays = progresses.map((progress, index) =>
-    Math.ceil((100 - progress) / speeds[index])
-  );
+  // 경우의 수 - 모든 의상을 안입는 경우(1)
+  const cases = Object.values(closet).reduce((a, c) => (a *= c + 1), 1) - 1;
+  return cases;
+};
 
-  let prev = leftDays[0];
-  let temp = 1;
+const clothes = [
+  ['yellow_hat', 'headgear'],
+  ['blue_sunglasses', 'eyewear'],
+  ['green_turban', 'headgear'],
+];
 
-  for (let i = 1; i < leftDays.length; i++) {
-    if (prev >= leftDays[i]) {
-      temp += 1;
-    } else {
-      answer.push(temp);
-      prev = leftDays[i];
-      temp = 1;
-    }
-  }
-  answer.push(temp);
-  return answer;
-}
-
-const progress = [93, 30, 55];
-const speeds = [1, 30, 5];
-solution(progress, speeds);
-
-// [2, 1]
+solution(clothes);
