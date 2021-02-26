24848c0d557c6a49eb65782be298ab279b6f7aa5
Suyeon Kang <tndusrkd92@gmail.com>
2021-02-26 11:59:58 +0900

    Frequency counter function

--- js/index.js
+++ js/index.js
@@ -1,24 +1,31 @@
-/* Programmers, 위장
-  Hash Table
-  서로 다른 옷의 조합의 수(모든 경우의 수)를 return
+/*
+두개의 array를 input으로 받아서, array1의 value가 array2의 squared value인지 확인하는 함수 
 */
 
-const solution = clothes => {
-  // 옷의 가짓수 세기
-  const closet = clothes.reduce((a, c) => {
-    a[c[1]] = a[c[1]] ? a[c[1]] + 1 : 1;
-    return a;
-  }, {});
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-  // 경우의 수 - 모든 의상을 안입는 경우(1)
-  const cases = Object.values(closet).reduce((a, c) => (a *= c + 1), 1) - 1;
-  return cases;
-};
+  let frequencyObj1 = {};
+  let frequencyObj2 = {};
 
-const clothes = [
-  ['yellow_hat', 'headgear'],
-  ['blue_sunglasses', 'eyewear'],
-  ['green_turban', 'headgear'],
-];
+  for (let key of arr1) {
+    frequencyObj1[key] = frequencyObj1[key] ? ++frequencyObj1[key] : 1;
+  }
+  for (let key of arr2) {
+    frequencyObj2[key] = frequencyObj2[key] ? ++frequencyObj2[key] : 1;
+  }
+  for (let key in frequencyObj1) {
+    if (frequencyObj1[key] !== frequencyObj2[key ** 2]) {
+      return false;
+    }
+    if (!(key ** 2 in frequencyObj2)) {
+      return false;
+    }
+  }
+  return true;
+}
 
-solution(clothes);
+// same([1, 2, 3, 2], [1, 4, 9, 4]); // true
+same([1, 2, 3, 2], [1, 4, 9, 5]); // false
