f715d00088594a5b8d92036b5aff7f6e226235ad
Suyeon Kang <tndusrkd92@gmail.com>
2021-10-10 15:38:16 +0900

    Binary Search

--- js/index.js
+++ js/index.js
@@ -1,19 +1,52 @@
-// Recursion
-// base case is so important!
+/* Binary Search
+ * sorted array and value
+ * linear search: O(n) vs binary search: O(log n)
+ */
 
-function power(base, exp) {
-  if (exp === 0) return 1;
-  if (exp === 1) return base;
-  return base * power(base, exp - 1);
-}
+// My Solution
+// const searchBinary = (arr, target) => {
+//   if (!arr.length) return;
 
-function factorial(num) {
-  if (num === 0) return 1;
-  if (num === 1) return num;
-  return num * factorial(num - 1);
-}
+//   const sorted = arr.sort();
+//   let start = 0;
+//   let end = sorted.length - 1;
+//   let middle = Math.floor(start + end) / 2;
 
-function productOfArray(arr) {
-  if (!arr.length) return 1;
-  return arr[0] * productOfArray(arr.slice(1));
-}
+//   if (sorted[middle] === target) {
+//     return middle;
+//   }
+
+//   while (start <= end) {
+//     if (sorted[middle] > target) {
+//       end = middle - 1;
+//       middle = Math.floor(start + end) / 2;
+//     } else if (sorted[middle] < target) {
+//       start = middle + 1;
+//       middle = Math.floor(start + end) / 2;
+//     } else if (sorted[middle] === target) {
+//       break;
+//     }
+//   }
+//   return middle;
+// };
+
+// Answer 1
+const searchBinary = (arr, target) => {
+  const sorted = arr.sort();
+  let start = 0;
+  let end = sorted.length - 1;
+  let middle = Math.floor(start + end) / 2;
+
+  while (sorted[middle] !== target && start <= end) {
+    if (sorted[middle] > target) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor(start + end) / 2;
+  }
+
+  return sorted[middle] === target ? middle : -1;
+};
+
+searchBinary([1, 3, 2, 4, 5, 7, 6], 5);
