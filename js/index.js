cc7dfff1a92a5afea0ae37c327a2325d5cf85e5f
Suyeon Kang <tndusrkd92@gmail.com>
2022-08-21 12:19:00 +0900

    백준1712: 손익분기점

--- js/index.js
+++ js/index.js
@@ -1,40 +1,19 @@
-/* Sorting: Merge Sort */
+/**
+ * 백준 1712번
+ * 손익분기점
+ * 입력: 첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 21억 이하의 자연수이다.
+ * 출력: 첫 번째 줄에 손익분기점 즉 최초로 이익이 발생하는 판매량을 출력한다. 손익분기점이 존재하지 않으면 -1을 출력한다.
+ * 
+ * 해결:
+ * 손익분기점 = 고정비용 / (제품 가격 - 가변 비용)
+ * C-B가 0 이하면 손익분기점은 없음
+ */
 
-const merge = (arr1, arr2) => {
-  let results = [];
-  let i = 0;
-  let j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-    if (arr2[j] > arr1[i]) {
-      results.push(arr1[i]);
-      i++;
-    } else {
-      results.push(arr2[j]);
-      j++;
-    }
-  }
-
-  while (i < arr1.length) {
-    results.push(arr1[i]);
-    i++;
-  }
-
-  while (j < arr2.length) {
-    results.push(arr2[j]);
-    j++;
-  }
-
-  return results;
+const getBreakEvenPoint = price => {
+  [A, B, C] = price.split(' ').map(p => +p);
+  const margin = Math.floor(A / (C - B)) + 1;
+  return margin < 0 ? -1 : margin;
 };
 
-const mergeSort = arr => {
-  if (arr.length <= 1) return arr;
+console.log(getBreakEvenPoint('3 2 1'));
 
-  let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-};
-
-mergeSort([2, 1, 4, 6, 0, 10]);
